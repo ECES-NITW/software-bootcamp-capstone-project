6c8cf9a246ff6bc0891b6036194bd4135ece7275
meghana413 <palavalasameghana1@gmail.com>
2026-07-20 18:50:43 +0530

    Done with authentication and authorization

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
 
 const connectDB = require("./config/Database");
 
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("Campus Marketplace Backend is Running!!");
 });
